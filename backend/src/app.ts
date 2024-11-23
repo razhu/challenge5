e7998986a33dd2d342a49365756c57a0d71a72c4
razhu <ramiro.huayller@10pearls.com>
2024-11-23 11:03:15 -0400

    update controller

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -12,7 +12,13 @@ dotenv.config();
 const app = express();
 const upload = multer({ dest: "uploads/" });
 
-app.post("/images", upload.single("image"), uploadImage);
+app.post("/images", upload.single("image"), (req, res, next) => {
+  try {
+    uploadImage(req, res);
+  } catch (error) {
+    next(error);
+  }
+});
 app.get("/images", listImages);
 app.get("/images/:width(\\d+)x:height(\\d+)/:s3_object", getResizedImage);
 
